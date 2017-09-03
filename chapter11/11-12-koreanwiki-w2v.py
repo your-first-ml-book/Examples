0e1647f1dfe56dd82c48753c7d819eb861d93114
figtart <raysylph@gmail.com>
2017-09-03 10:34:50 -0700

    Update 11-12-koreanwiki-w2v.py

--- chapter11/11-12-koreanwiki-w2v.py
+++ chapter11/11-12-koreanwiki-w2v.py
@@ -5,8 +5,6 @@ import unicodedata
 from konlpy.tag import Mecab
 from gensim.models import Word2Vec
 
-hangul_start = 44032 #가
-hangul_end = 55203 #힣
 
 #parameters
 WINDOW=5
@@ -15,25 +13,30 @@ BATCH_SIZE = 10000
 ITER = 10
 
 def read_text(fin):
+    # 전처리된 위키백과 파일을 읽어 들입니다.
     corpus_li = []
     mecab = Mecab(dicpath='/opt/local/lib/mecab/dic/mecab-ko-dic')
     for line in open(fin):
+        # 깨지는 글자를 처리하기 위해 unicodedata.normalize 함수를 이용해 
+        # NFKC로변환합니다.
         line = unicodedata.normalize('NFKC', line)
         try:
+            # 첫 글자가 숫자로 시작하는 문장을 말뭉치에 추가합니다.
             _ = int(line[0])
             corpus_li.append(' '.join(mecab.nouns(line)) + '\n')
 
         except ValueError:
-            if ord(line[0]) >= hangul_start and ord(line[0]) <= hangul_end:
+            # 첫 글자가 한글로 시작하는 문장을 말뭉치에 추가합니다.
+            if ord(line[0]) >= ord('가') and ord(line[0]) <= ord('힇'):
                 corpus_li.append(' '.join(mecab.nouns(line))+'\n')
             else:
                 pass
-
+    print('# of lines in corpus',len(corpus_li))
     return(corpus_li)
 
 
 def train_word2vec(corpus_li, fout_model):
-
+    # read_text에서 생성한 말뭉치를 이용해 word2vec을 학습시킵니다.
     model = Word2Vec(corpus_li, sg=1, size=EMBEDDING_SIZE, window=WINDOW, min_count=5, workers=3, batch_words=BATCH_SIZE, iter=ITER)
     model.init_sims(replace=True) #clean up memory
     model.save(fout_model)
@@ -41,19 +44,17 @@ def train_word2vec(corpus_li, fout_model):
 
 
 
+# 전처리된 파일을 한번에 읽어 들이기 위한 정규식
+input_pattern = '/Users/forumai/Downloads/kor_wiki/kowiki-latest-pages-articles.xml-88.txt'
+fin_li = glob.glob(input_pattern)
 
-if __name__ == '__main__':
+for fin in fin_li:
+    corpus_li = read_text(fin)
 
-    argv = sys.argv
-
-    input_pattern = '/Users/forumai/Downloads/kor_wiki/kowiki-latest-pages-articles.xml-88.txt'
-    fin_li = glob.glob(input_pattern)
-
-    for fin in fin_li:
-        corpus_li = read_text(fin)
-        model = train_word2vec(corpus_li, '/Users/forumai/Downloads/kor_wiki/test_model.txt')
-        print(model.most_similar('프랑스', topn=20))
-        print(model.most_similar(positive=['한국','파리'], negative=['서울']))
+# 모델학습
+model = train_word2vec(corpus_li, '/Users/forumai/Downloads/kor_wiki/test_model.txt')
+print(model.most_similar('프랑스', topn=20))
+print(model.most_similar(positive=['한국','파리'], negative=['서울']))
 
 
 
