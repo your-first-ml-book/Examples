3e0066cdfc61c6467a6ed7d8b3c217e6c6c9f06b
Seungyeon Kim <sylund@gmail.com>
2017-09-10 11:37:55 -0700

    Typo

--- chapter11/11-8-named-entity-recognition.py
+++ chapter11/11-8-named-entity-recognition.py
@@ -7,7 +7,7 @@ STANFORD_NER_JAR_PATH = '압축을 푼 장소/stanford-ner-3.6.0.jar'
 ner_tagger = StanfordNERTagger(STANFORD_NER_CLASSIFER_PATH, STANFORD_NER_JAR_PATH)
 
 # 임의로 만든 예제입니다. 이 부분을 원하는 문장으로 바꿔서 실습하세요.
-text = 'One day in November 2016, the two authors of this book, Seungyeon and Youngjoo, had a coffee at Red Rock cafe, which is very popular place in Mountain View.'
+text = 'One day in November 2016, the two authors of this book, Seungyeon and Youngjoo, had a coffee at Red Rock cafe, which is a very popular place in Mountain View.'
 
 tokens = word_tokenize(text)
 print(ner_tagger.tag(tokens))
