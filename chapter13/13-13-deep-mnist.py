0a3c4e07eaf30042fc7c10863c32c32dc128ffcd
figtart <raysylph@gmail.com>
2017-09-27 22:30:55 -0700

    Update 13-13-deep-mnist.py

--- chapter13/13-13-deep-mnist.py
+++ chapter13/13-13-deep-mnist.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+
 import argparse
 import sys
 from tensorflow.examples.tutorials.mnist import input_data
