52260a863356119b55b96641e9dbcef7525ddb8d
figtart <raysylph@gmail.com>
2019-02-24 00:39:41 -0800

    updated line 8 from len(v)<=600 to len(v)<600

--- chapter10/10-03-remove-outliers.py
+++ chapter10/10-03-remove-outliers.py
@@ -5,7 +5,7 @@ max_product_user_li =[k for k,v in user_product_dic.items() if len(v)>=600]
 print("# of users purchased one product:%d" % (len(min_product_user_li)))
 print("# of users purchased more than 600 product:%d" % (len(max_product_user_li)))
 # 찾아낸 사용자를 군집화에 사용할 user_product_dic에서 제외합니다.
-user_product_dic = {k:v for k,v in user_product_dic.items() if len(v)>1 and len(v)<=600}
+user_product_dic = {k:v for k,v in user_product_dic.items() if len(v)>1 and len(v)<600}
 print("# of left user:%d" % (len(user_product_dic)))
 # 남아 있는 사용자가 구매한 상품에도 0에서 시작하는 고유 ID를 부여합니다.
 # 데이터셋에서 제외된 사용자가 구매한 상품은 군집화에서 사용하지 않기 때문에 이러한 처리를 해야 합니다.
